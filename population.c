263148f1c56e31c59440d2756f9a92f7519d2016
Daniel Ramirez <67478253+DRamirezGAAQ@users.noreply.github.com>
2021-11-09 10:58:10 +0300

    Update population.c

--- population.c
+++ population.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include <cs50.h>
-
+/*
+    This code compiles and returns the correct output.
+    This code has style issues, mainly around indentation
+    Content inside of curly braces should be indented once,
+    and should remain at that level of indentation unless a new
+    set of braces appear. You code has several levels of indentation
+    for no reason. 
+*/
 int main(void) {
-    int n;
+    int n; //corecct indentation
     
     
-        n = get_int("Start Population: ");
+        n = get_int("Start Population: "); //incorecct indentation
     
       
-      int ans = n + (n/3) - (n/4);
+      int ans = n + (n/3) - (n/4); //incorecct indentation
       
-      printf("End population after one year: %i\n", ans);
+      printf("End population after one year: %i\n", ans); //incorecct indentation
 }
